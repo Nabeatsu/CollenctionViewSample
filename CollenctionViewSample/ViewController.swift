2a8445027bab838612daa033f105aba7c34e3d7c
tanabenobuyuki <tanabe.1478@gmail.com>
2018-11-27 15:13:33 +0900

    Initial Commit

---
+++ CollenctionViewSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollenctionViewSample
+//
+//  Created by 田辺信之 on 2018/11/27.
+//  Copyright © 2018年 田辺信之. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
