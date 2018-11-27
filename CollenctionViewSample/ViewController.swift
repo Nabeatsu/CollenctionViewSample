7de75ef9371e8c728fec5378728e0e3a7047ef58
tanabenobuyuki <tanabe.1478@gmail.com>
2018-11-27 16:51:30 +0900

    feat: UICollectionViewのサンプルを実装

--- CollenctionViewSample/ViewController.swift
+++ CollenctionViewSample/ViewController.swift
@@ -8,12 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    @IBOutlet weak var collectionView: UICollectionView!
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 400
+    }
+    
+
+
+
+    // セルを返す
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        
+        //セルの背景色をランダムに設定する。
+        cell.backgroundColor = UIColor(red: CGFloat(drand48()),
+                                       green: CGFloat(drand48()),
+                                       blue: CGFloat(drand48()),
+                                       alpha: 1.0)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("押された")
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        UINib(nibName: "UICollectionElementKindCell", bundle:nil)
     }
+
+    // UICollectionViewの外周余白
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    // Cellのサイズ
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellSize : CGFloat = 100
+        return CGSize(width: cellSize, height: cellSize)
+    }
+    // 行の最小余白
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    // 列の最小余白
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
 
 
 }
